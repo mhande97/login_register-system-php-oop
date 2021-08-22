a43c14dd0a56d6daec69e801bbe0d42ce22b1f13
mhande_amalou <80551146+mhande97-dz@users.noreply.github.com>
2021-08-22 13:59:15 +0100

    add insert and update method

--- classes/DB.php
+++ classes/DB.php
@@ -83,13 +83,6 @@ class DB {
             
         }
 
-
-
-
-        public function Action()
-        {
-            #under validation
-        }
         /**
          * public Get method v0.1
          * method that Form the Get Query
@@ -124,8 +117,86 @@ class DB {
           endif;
           
         }
-        
-        public function Delete($table,$condition,$params = array())
+        /**
+         * Insert method v0.1
+         * insert data on the database
+         * accept paramerter
+         * $table = the table to insert
+         * $fields = an array hold fields_name as keys and value
+         */
+        public function Insert($table,$fields = array())
+        {
+            if (count($fields)):
+
+
+                $keys = array_keys($fields);
+                 
+                $columns = "`" . implode("`,`",$keys) . "`";
+                $Que_mark = "";
+                  
+                foreach ($fields as $item):
+                    $Que_mark .= "?,";
+                endforeach;
+                $Que_mark = trim($Que_mark,",");
+                
+                
+               $sql = "INSERT INTO $table($columns) VALUES ($Que_mark)";
+
+               if (!$this->Query($sql,$fields)->_error):
+                   return $this;
+               endif;
+               
+               endif;
+               return false;
+        }
+          /**
+           * update method v0.1
+           * update data on the database
+           * Accept Parameters
+           * $table = the table to update
+           * $fields = an array hold fields_name as keys and value[update]
+           * $where = row will be updated
+           */
+        public function Update($table,$fields,$where = array('itemID' => 0 ))
+        {
+            
+            if (count($fields) && count($where)):
+                
+
+                // $keys = array_keys($fields);
+                $where_key = array_keys($where)[0];
+               
+
+                $Set = "";
+                foreach ($fields as $key => $value):
+                    $Set .= $key . " = ? ," ;
+                endforeach;
+                $Set = trim($Set,",");
+                 
+
+                $sql = "UPDATE $table SET $Set WHERE $where_key = ?";
+
+               
+                 $fields = array_merge($fields,$where);
+                 
+
+                if (!$this->Query($sql,$fields)->_error):
+                    return $this;
+                endif;
+               
+            endif;
+
+            return false;
+        }
+
+        /**
+         * delete method v0.1
+         * Accept Parameters
+         * $table = the table to delete from
+         * $condition = [where clasure]
+         * $params = array Holds values that will be bind with sql query
+         */
+        public function Delete($table,$condition,$params = array(0))
         {
             $sql = "DELETE FROM $table WHERE $condition";
             
@@ -135,7 +206,6 @@ class DB {
         }
 
         
-
          public function Result()
          {
              return $this->_result;
