c969f29033e420e56445ce0f68f2448c74b6049b
mhande_amalou <80551146+mhande97-dz@users.noreply.github.com>
2021-08-22 23:22:13 +0100

    update escape function

--- functions/Sanitize.php
+++ functions/Sanitize.php
@@ -1,7 +1,28 @@
 <?php
 
-function escape($string)
+function escape($string,$type)
 {
-    return filter_var($string,FILTER_SANITIZE_STRING);
+
+    switch ($type) {
+        case 'string':
+        case 'text':
+            return filter_var($string,FILTER_SANITIZE_STRING);
+            break;
+        case 'number':
+        case 'integer':
+            return filter_var($string,FILTER_SANITIZE_NUMBER_INT);
+            break;
+        case 'email':
+            return filter_var($string,FILTER_SANITIZE_EMAIL);
+            break;
+        
+        
+        
+        default:
+        return filter_var($string,FILTER_SANITIZE_SPECIAL_CHARS);
+            break;
+    }
+    
+
    
 }
